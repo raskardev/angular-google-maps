bf586172d06091a83166e54bff1e98595a7336ce
raskardev <alexcdev95@gmail.com>
2020-01-24 20:28:23 +0100

    chore(): add entryComponents

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
 import { MapComponent } from './components/map/map.component';
+import { EditMapComponent } from './components/map/edit-map/edit-map.component';
 
 import { AgmCoreModule } from '@agm/core';
 
 @NgModule({
-  declarations: [AppComponent, MapComponent],
+  entryComponents: [EditMapComponent],
+  declarations: [AppComponent, MapComponent, EditMapComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    ReactiveFormsModule,
     MaterialModule,
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyCtbiDnyJ2I56agv0vPgSnI2t6yJXe4mXw'
