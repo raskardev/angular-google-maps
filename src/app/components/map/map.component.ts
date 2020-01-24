0295d10ad2d3a6fdbab03511a6423286330e2985
raskardev <alexcdev95@gmail.com>
2020-01-24 19:23:48 +0100

    feat(): create addMarker and saveStorage methods

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Marker } from '../../models/marker.model';
 
 @Component({
   selector: 'app-map',
@@ -6,10 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./map.component.css']
 })
 export class MapComponent implements OnInit {
+  markers: Marker[] = JSON.parse(localStorage.getItem('markers')) || [];
+
   lat = 36.6776482;
   lng = -6.1420956;
 
-  constructor() {}
+  constructor() {
+    const marker = new Marker(this.lat, this.lng);
+
+    this.markers.push(marker);
+  }
 
   ngOnInit() {}
+
+  addMarker({ coords: { lat, lng } }) {
+    const marker = new Marker(lat, lng);
+
+    this.markers.push(marker);
+
+    this.saveStorage();
+  }
+
+  saveStorage() {
+    localStorage.setItem('markers', JSON.stringify(this.markers));
+  }
 }
